b6ce3ea395c0d39fc1b4644d8be158ffb5b7556b
Dominic Michaud <dominic@ateliermade.ca>
2017-01-19 14:20:01 -0500

    added user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = Author.new
+user.username = 'admin'
+user.email = 'admin@example.com'
+user.encrypted_password = 'password'
+user.save!
